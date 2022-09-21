0366d0172fa6c5284d181807a36b7a8c18c8331b
Xica Felix <xicasfelix@gmail.com>
2022-09-21 14:38:32 -0400

    commit tree 74c74408f08a99b5fff5769c8c9c8f96f5a6862a

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,23 @@
 // create your App component here
+
+import React, {useEffect, useState } from "react";
+
+function App(){
+    const [data, setData] = useState('')
+    const [isLoaded, setLoaded] = useState(false)
+    useEffect(()=>{
+        fetch('https://dog.ceo/api/breeds/image/random').then(response=> response.json()).then(result=>{
+            setLoaded(true);
+            setData(result);
+            console.log(data)
+        })
+    }, [])
+    if(!isLoaded) return <h3>Loading...</h3>
+    return(
+        <div>
+            <img src={data.message} alt='A Random Dog'/>
+        </div>
+    );
+}
+
+export default App;
